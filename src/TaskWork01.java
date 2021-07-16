d86220965eb68317f4cabef1cecd60afba7c264c
elnar87 <ifg1@yandex.ru>
2021-07-16 18:03:03 +0300

    Добавил описание задачи

--- src/TaskWork01.java
+++ src/TaskWork01.java
@@ -1,3 +1,10 @@
+/*
+Вывести на консоль текст “Hello World!”:
+Скачать и установить IntelliJ IDEA.
+Написать первую программу, которая выводит в консоль строку “Hello World!”.
+Подключиться к GitHub, залить программу в созданный репозиторий.
+ */
+
 public class TaskWork01 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
