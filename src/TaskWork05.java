ab1f9eebd0cb58712c44d880f2134a216963c967
elnar87 <ifg1@yandex.ru>
2021-08-17 08:44:39 +0300

    Задача №5

---
+++ src/TaskWork05.java
@@ -0,0 +1,34 @@
+/*
+Напишите простой калькулятор:
+
+Ввести 2 числа с клавиатуры, затем один из символов ‘+’, ‘-’, ‘*’ или ‘/’.
+В зависимости от символа вывести в консоль результат выполнения соответствующей арифметической операции.
+ */
+
+import java.sql.SQLOutput;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class TaskWork05 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String expression = scanner.nextLine();
+
+        StringTokenizer stringTokenizer = new StringTokenizer(expression);
+
+        int a = Integer.parseInt(stringTokenizer.nextToken());
+        int b = Integer.parseInt(stringTokenizer.nextToken());
+        String operator = stringTokenizer.nextToken();
+
+        int result = 0;
+        switch (operator) {
+            case "+":
+                result = a + b;
+                break;
+        }
+
+        System.out.println(result);
+    }
